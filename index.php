156353646b011f9eebaaf875d7d7e6a54e14d8a0
angelo-simoes <angelo.simoes@edu.univ-fcomte.fr>
2023-11-24 15:00:20 +0100

    installation Tailwind

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <?php wp_head(); ?>
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="all" />
+  </head>
+  <body>
+    <p class="text-orange-500 bg-slate-300">Bonjour !</p>
+    <?php
+    if ( have_posts() ) {
+      while ( have_posts() ) {
+        the_post();
+        the_title( '<h1 class="entry-title">', '</h1>' );
+        ?>
+        <div class="entry-content">
+          <?php the_content(); ?>
+        </div>
+        <?php
+      }
+    }
+    ?>
+  </body>
+</html>
